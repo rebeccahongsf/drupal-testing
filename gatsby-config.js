1a885c359a6d7b212f977d25e31232e17576af25
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-12-24 14:06:47 -0800

    Test GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
 module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
