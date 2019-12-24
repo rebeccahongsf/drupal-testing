be8f53c73e2bf550eb2bedb4f88af40c0b3d0ea8
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-12-24 10:42:32 -0800

    Update layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,9 @@
 import React from "react"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Header 
-      headerText="About Gatsby" // passes through and is used through {props.headerText}
-      arbitraryPhrase=" is arbitrary"
-    />
-    <Header headerText="Second Header!"/>
-    <p>Such wow. Very React.</p>
-  </div>
+  <Layout>
+    <h1>About me</h1>
+    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+  </Layout>
 )
