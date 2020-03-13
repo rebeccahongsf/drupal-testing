a1e0cd7516c60b10dfb2c617e2d31a7773eec4ae
Rebecca Hong <rebeccahongsf@gmail.com>
2020-03-12 21:56:25 -0700

    Fix blog-post graphql query for image path

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,31 +9,31 @@ export default ({ data }) => {
       <div>
         <h1>{post.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.body.value }} />
-        <img src={post.relationships.field_image.localFile.absolutePath} alt={post.field_image.alt} height={post.field_image.height} width={post.field_image.width} />
+        <img src={post.relationships.field_image.localFile.publicURL} alt={post.field_image.alt} height={post.field_image.height} width={post.field_image.width} />
       </div>
     </Layout>
   )
 }
 
 export const query = graphql`
-query ($slug: String!) {
-  nodeArticle(path: {alias: {eq: $slug}}) {
-    body {
-      value
-    }
-    field_image {
-      width
-      height
-      alt
-    }
-    relationships {
+  query ($slug: String!) {
+    nodeArticle(path: {alias: {eq: $slug}}) {
+      body {
+        value
+      }
       field_image {
-        localFile {
-          absolutePath
+        width
+        height
+        alt
+      }
+      relationships {
+        field_image {
+          localFile {
+            publicURL
+          }
         }
       }
+      created
     }
-    created
   }
-}
 `
